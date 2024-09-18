0a9a91190a79c387202a69e991f702ac5722ac06
codefrofun <blackpinkaricat@gmail.com>
2024-09-18 12:50:53 -0300

    Initial commit

---
+++ Time_Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = 8926548;
+            int min = sec / 60;
+            int hours = min / 60;
+            int day = hours / 24;
+            int month = day / 30;
+            int season = month % 4;
+
+            string[] seasons =
+            {
+                "Spring", "Summer", "Fall", "Winter"
+            };
+
+            Console.WriteLine(sec);
+            Console.WriteLine((hours % 24) + ":" + (min % 60));
+            Console.WriteLine();
+            Console.WriteLine(day % 30);
+            Console.WriteLine(season);
+            
+
+                int hour24 = 13; // Example input
+                int hour12 = hour24 % 12;
+
+                // Handle special case for midnight and noon
+                if (hour12 == 0)
+                {
+                    hour12 = 12; // Convert 0 to 12 for 12 AM or 12 PM
+                }
+
+                // Determine AM or PM
+                string period = hour24 >= 12 ? "PM" : "AM";
+
+                Console.WriteLine($"Time of day: {hour12} {period}");
+                Console.ReadLine();
+        }
+    }
+}
---
+++ Time_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Time_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("Time_Test")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e30fe83-7d6e-44a3-914a-2b722f9f318d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
